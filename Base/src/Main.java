5f8f571e39355ec563fee32f9376ea7cda1329b1
foruser <vinhpham2761.gmail.com>
2023-08-28 06:17:41 +0700

    buoi 1 base

---
+++ Base/src/Main.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        //vong lap: for
+        for (; ; ) {
+            System.out.println( ". " +"Toi xin loi");
+        }
+        
+        
+        //while
+//        while (true) {            
+//            System.out.println("Toi xin loi");
+//            int a = 10;
+//        }
+
+    }
+}
