fdc24a8b1728dad0f82ec1b378299724416d75b7
foruser <vinhpham2761.gmail.com>
2023-10-03 18:05:40 +0700

    car show room OOP

---
+++ 0066 - Car - OOP/src/bo/ExceptionCar.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bo;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class ExceptionCar extends Exception{
+      public ExceptionCar() {
+    }
+
+    public ExceptionCar(String message) {
+        super(message);
+    }
+}
